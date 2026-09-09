6a965285328b371c245fc8a91c8cbf0e9d1b0cdc
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-09-09 20:20:25 +0100

    chore: remove payment service from public app

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,5 @@ export { authService } from './auth-service.js';
 export { academicService } from './academic-service.js';
 export { notificationService } from './notification-service.js';
 export { supportService } from './support-service.js';
-export { paymentService } from './payment-service.js';
 export { reportService } from './report-service.js';
 export { adminService } from './admin-service.js';
